3642f3020dccdd3dfc4cc32a90f77ab54bdda4ae
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-05-14 11:36:21 +0100

    General navbar template

--- app/components/navigation-bar/navigation-bar.component.js
+++ app/components/navigation-bar/navigation-bar.component.js
@@ -1,10 +1,18 @@
-import template from './navigation-bar.template.html';
+import template from './navigation-bar.template.pug';
 
 export default {
   template,
   controller: class {
 
     constructor($scope, $location) {
+      this.pageTitles = {
+        add: 'Ajouter une nouvelle oeuvre',
+        changelog: 'Changelog',
+        edit: 'Éditer une oeuvre',
+        search: 'Rechercher une oeuvre',
+        settings: 'Paramètres',
+      };
+
       $scope.$watch(() => $location.path(), (route) => {
         const words = route.split('/');
         this.route = words[1];
