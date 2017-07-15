ba05ffbf8c300e1cb20360f2d9e2a4d69c2723f3
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-07-15 18:53:02 +0200

    Clean React dependencies

--- app/components/navigation-bar/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import NavigationBarComponent from './navigation-bar.component';
-import Version from '../version';
-
-export default angular.module('myApp.navigationBar', [
-  Version.name,
-])
-    .component('navigationBar', NavigationBarComponent);
