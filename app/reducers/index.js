8f41c5f3037483c3a6df8216ecdf5b81cab0e492
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-11-04 14:28:32 +0000

    Use standard boilerplate

--- app/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import works from './works';
-
-export default combineReducers({
-  works
-});
--- app/reducers/index.test.js
+++
@@ -1,7 +0,0 @@
-import reducer from './index';
-
-describe('The main reducer', () => {
-  it('initializes a defined value', () => {
-    expect(reducer(undefined, {})).toBeDefined();
-  });
-});
