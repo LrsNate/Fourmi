e770fd483601d827681d90ef05dfad36f805627d
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-11-04 17:29:18 +0000

    Fix ESLint config, disable store persistance

--- app/store.js
+++ app/store.js
@@ -5,7 +5,6 @@ import {
 } from "react-router-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import persistState from "redux-localstorage";
 import thunk from "redux-thunk";
 
 import { loginAction } from "./actions/user";
@@ -28,10 +27,7 @@ export default function configureStore(initialState, routerHistory) {
 
   const composeEnhancers = composeWithDevTools({ actionCreators });
 
-  const enhancer = composeEnhancers(
-    applyMiddleware(...middlewares),
-    persistState()
-  );
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
   const rootReducer = combineReducers(reducers);
 
   return createStore(rootReducer, initialState, enhancer);
