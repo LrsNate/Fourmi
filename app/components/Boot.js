839ace926b1f9f817c9a8ad81abd065b50f75d75
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2017-11-14 22:30:33 +0000

    Remove unused imports

--- app/components/Boot.js
+++ app/components/Boot.js
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Redirect } from "react-router";
 import { push } from "react-router-redux";
 
 import {
