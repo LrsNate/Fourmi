b4459ae420aeee75c59fde8aa0fa07069d2e571b
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2018-09-14 13:02:39 +0200

    Add notes and submit button to epigram editor

--- src/renderer/components/EpigramEditor.tsx
+++ src/renderer/components/EpigramEditor.tsx
@@ -1,22 +1,46 @@
-import { Card, CardContent, CardHeader, Grid } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  StyleRulesCallback,
+  Theme,
+  withStyles
+} from "@material-ui/core";
+import { Save } from "@material-ui/icons";
 import * as React from "react";
 import { Form } from "react-final-form";
+import { RouteComponentProps, withRouter } from "react-router";
 import { Epigram } from "../constants/types";
+import { searchRoutePath } from "../routes";
 import TextEditor from "./forms/TextEditor";
 import TextInput from "./forms/TextInput";
 
-interface EpigramEditorProps {
+const styles: StyleRulesCallback<string> = (theme: Theme) => ({
+  saveButton: {
+    position: "fixed",
+    right: theme.spacing.unit * 2,
+    bottom: theme.spacing.unit * 2,
+    zIndex: 1
+  }
+});
+
+interface EpigramEditorProps extends RouteComponentProps<{}> {
   epigram: Epigram;
   onSave: (values: Epigram) => void;
+  classes: { saveButton: string };
 }
 
 class EpigramEditor extends React.Component<EpigramEditorProps> {
   public handleSave = (values: object) => {
-    this.props.onSave(values as Epigram);
+    const { history, onSave } = this.props;
+    onSave(values as Epigram);
+    history.push(searchRoutePath());
   };
 
   public render() {
-    const { epigram } = this.props;
+    const { classes, epigram } = this.props;
 
     return (
       <Form initialValues={epigram} onSubmit={this.handleSave}>
@@ -30,30 +54,28 @@ class EpigramEditor extends React.Component<EpigramEditorProps> {
               <Grid item={true}>
                 {this.renderTextEditor("Texte français", "frenchText")}
               </Grid>
+              <Grid item={true}>{this.renderTextEditor("Notes", "notes")}</Grid>
             </Grid>
+            <Button
+              className={classes.saveButton}
+              type="submit"
+              variant="fab"
+              color="secondary"
+            >
+              <Save />
+            </Button>
           </form>
         )}
       </Form>
     );
   }
 
-  public renderTextEditor(label: string, name: string) {
-    return (
-      <Card>
-        <CardHeader title={label} />
-        <CardContent>
-          <TextEditor name={name} />
-        </CardContent>
-      </Card>
-    );
-  }
-
   public renderAttributes() {
     return (
-      <Card className={"?"}>
+      <Card>
         <CardHeader title="Propriétés" />
         <CardContent>
-          <Grid container={true}>
+          <Grid container={true} spacing={8}>
             <Grid item={true} sm={3}>
               <TextInput label="Auteur" name="author" />
             </Grid>
@@ -83,6 +105,17 @@ class EpigramEditor extends React.Component<EpigramEditorProps> {
       </Card>
     );
   }
+
+  public renderTextEditor(label: string, name: string) {
+    return (
+      <Card>
+        <CardHeader title={label} />
+        <CardContent>
+          <TextEditor name={name} />
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
-export default EpigramEditor;
+export default withStyles(styles)(withRouter(EpigramEditor));
